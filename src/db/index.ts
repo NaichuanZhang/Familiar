c26a8e5a78a89f81b682102eb589473bc9a249ee
Alex Zhang <zhangnai@amazon.com>
2026-03-27 16:17:40 -0700

    fix: add ssl require for Ghost PostgreSQL connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,6 @@ import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, { ssl: "require" });
 
 export const db = drizzle(client, { schema });
